dc2a891e14aa5a510c2c7c09b10cf421791fe1c7
Dima Lazarev <dmitry.lazarev@gmail.com>
2025-07-27 00:58:43 +0300

    Solve Day 5

---
+++ src/day05.rs
@@ -0,0 +1,81 @@
+use aoc_runner_derive::aoc;
+use std::collections::HashMap;
+
+#[aoc(day5, part1)]
+fn part1(polymer: &str) -> usize {
+    let mut stack = Vec::new();
+
+    for unit in polymer.chars() {
+        if stack.is_empty() {
+            stack.push(unit);
+        } else {
+            let last = stack.last().unwrap();
+
+            if last.eq_ignore_ascii_case(&unit)
+                && (last.is_lowercase() && unit.is_uppercase()
+                    || last.is_uppercase() && unit.is_lowercase())
+            {
+                stack.pop();
+            } else {
+                stack.push(unit);
+            }
+        }
+    }
+
+    stack.len()
+}
+
+#[aoc(day5, part2)]
+fn part2(polymer: &str) -> usize {
+    let mut stacks = HashMap::new();
+
+    for unit in 'a'..='z' {
+        stacks.insert(unit, Vec::new());
+    }
+
+    for unit in polymer.chars() {
+        for (index, stack) in stacks.iter_mut() {
+            if unit.eq_ignore_ascii_case(index) {
+                continue;
+            }
+
+            if stack.is_empty() {
+                stack.push(unit);
+            } else {
+                let last = stack.last().unwrap();
+
+                if last.eq_ignore_ascii_case(&unit)
+                    && (last.is_lowercase() && unit.is_uppercase()
+                        || last.is_uppercase() && unit.is_lowercase())
+                {
+                    stack.pop();
+                } else {
+                    stack.push(unit);
+                }
+            }
+        }
+    }
+
+    stacks
+        .values()
+        .min_by(|x, y| x.len().cmp(&y.len()))
+        .unwrap()
+        .len()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static TEST_INPUT: &str = "dabAcCaCBAcCcaDA";
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(TEST_INPUT), 10);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(TEST_INPUT), 4);
+    }
+}
